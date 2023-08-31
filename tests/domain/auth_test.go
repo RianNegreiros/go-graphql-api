e07f2938f152de38cc6ad1a5439995b84d121d0a
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-08-31 13:56:28 -0300

    test: email taken

--- tests/domain/auth_test.go
+++ tests/domain/auth_test.go
@@ -67,4 +67,24 @@ func TestAuthService_Register(t *testing.T) {
 		userRepo.AssertNotCalled(t, "Create")
 		userRepo.AssertExpectations(t)
 	})
+
+	t.Run("email taken", func(t *testing.T) {
+		ctx := context.Background()
+
+		userRepo := &mocks.UserRepo{}
+
+		userRepo.On("GetByUsername", mock.Anything, mock.Anything).
+			Return(internal.User{}, internal.ErrNotFound)
+
+		userRepo.On("GetByEmail", mock.Anything, mock.Anything).
+			Return(internal.User{}, nil)
+
+		service := domain.NewAuthService(userRepo)
+
+		_, err := service.Register(ctx, validInput)
+		require.ErrorIs(t, err, internal.ErrEmailTaken)
+
+		userRepo.AssertNotCalled(t, "Create")
+		userRepo.AssertExpectations(t)
+	})
 }
